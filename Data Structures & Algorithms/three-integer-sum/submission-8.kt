e8ab63c1f9076b5e362d3ce3559ca60978997109
Rahul Agrahari <android.rahul.agrahari@gmail.com>
2026-09-21 21:50:17 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.kt
@@ -0,0 +1,33 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
+       val set = mutableSetOf<List<Int>>()
+       for((i,num) in nums.withIndex()){
+         if(num > 0)
+           break
+         var left = i+1
+         var right = nums.lastIndex
+         while(left < right){
+            val sum = num + nums[left] + nums[right]
+            when{
+                sum > 0 -> {
+                    right--
+                }
+
+                sum < 0 -> {
+                    left++
+                }
+
+                else ->{
+                    set.add(listOf(num,nums[left],nums[right]))
+                    left++
+                    right--
+                }
+            }
+         }  
+       }
+       return set.map{
+        it.toList()
+       }
+    }
+}
